60ec8cab7b90a353591693d301e88ddb98591b44
Adaso <adaso578@student.liu.se>
2014-11-26 19:10:04 +0100

    Animation och ljudeffekter

--- app/src/main/java/com/example/adamsoderstrom/jagharaldrig/Main.java
+++ app/src/main/java/com/example/adamsoderstrom/jagharaldrig/Main.java
@@ -2,12 +2,16 @@ package com.example.adamsoderstrom.jagharaldrig;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import static android.widget.Button.*;
 
@@ -20,7 +24,12 @@ public class Main extends Activity {
         setContentView(R.layout.activity_main);
 
         ImageButton btnStart = (ImageButton) findViewById(R.id.startButton);
+        ImageView logo = (ImageView) findViewById(R.id.imageView);
 
+        logo.startAnimation(AnimationUtils.loadAnimation(Main.this, R.anim.fade_in1));
+        btnStart.startAnimation(AnimationUtils.loadAnimation(Main.this, R.anim.fade_in2 ));
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.wooden_stick_fast_swipe_through_air_version_2);
 
         btnStart.setOnClickListener(new OnClickListener() {
 
@@ -28,6 +37,8 @@ public class Main extends Activity {
             public void onClick(View v) {
                 Intent i = new Intent(Main.this, runActivity.class);
                 startActivity(i);
+
+                mp.start();
 
                 overridePendingTransition(R.anim.right_in, R.anim.left_out);
 
